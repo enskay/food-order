e9dad4d1d9127d9d81f39dc28fa257b39c9756f7
enskay <bus.eneskay@gmail.com>
2023-03-22 13:49:07 +0300

    Sepetten Silme İslemi

--- client/src/reducers/cartReducers.js
+++ client/src/reducers/cartReducers.js
@@ -5,6 +5,14 @@ export const addToCartReducer = (state = { cartItems: [] }, action) => {
         ...state,
         cartItems: [...state.cartItems, action.payload],
       };
+
+    case "DELETE_FROM_CART":
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(
+          (sepet) => sepet._id !== action.payload._id
+        ),
+      };
     default:
       return state;
   }
